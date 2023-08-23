fb656dc3a2eb4e3d88926d11974c6b9f78cb9cb3
trungtn <trungtn@vmodev>
2023-08-23 10:01:03 +0700

    fix/layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -27,7 +27,8 @@
       </ion-toolbar>
     </ion-header>
     <ion-grid>
-      <ion-card class="item-card">
+      <ion-row>
+        <ion-card class="item-card">
           <ion-card-header>
             <ion-card-title class="card-title">Trưa ngày 22/08</ion-card-title>
           </ion-card-header>
@@ -38,12 +39,12 @@
                 alt="The Wisconsin State Capitol building in Madison, WI at night"
             >
             </ion-img>
-            <div>
-              <div>Loại : Cơm , cơm rang, bún </div>
-              <div>Giá trung: 25k-50k</div>
+            <div class="menu-info">
+              <div>Loại: Cơm , cơm rang, bún </div>
+              <div>Giá trung bình: 25k-50k</div>
               <div>Thời gian order: 11h50- 12h30</div>
               <div>Số lượng giới hạn: 30 suất</div>
-            </div>g
+            </div>
           </ion-card-content>
           <ion-card-content class="card-title">
             <ion-button>
@@ -51,6 +52,79 @@
             </ion-button>
           </ion-card-content>
         </ion-card>
+        <ion-card class="item-card">
+          <ion-card-header>
+            <ion-card-title class="card-title">Trưa ngày 22/08</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-img
+                class="img-card"
+                src="https://f18-zpc.zdn.vn/8038687805436259717/f801ad2366f4b4aaede5.jpg"
+                alt="The Wisconsin State Capitol building in Madison, WI at night"
+            >
+            </ion-img>
+            <div class="menu-info">
+              <div>Loại: Cơm , cơm rang, bún </div>
+              <div>Giá trung bình: 25k-50k</div>
+              <div>Thời gian order: 11h50- 12h30</div>
+              <div>Số lượng giới hạn: 30 suất</div>
+            </div>
+          </ion-card-content>
+          <ion-card-content class="card-title">
+            <ion-button>
+              Đặt
+            </ion-button>
+          </ion-card-content>
+        </ion-card>
+        <ion-card class="item-card">
+          <ion-card-header>
+            <ion-card-title class="card-title">Trưa ngày 22/08</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-img
+                class="img-card"
+                src="https://f18-zpc.zdn.vn/8038687805436259717/f801ad2366f4b4aaede5.jpg"
+                alt="The Wisconsin State Capitol building in Madison, WI at night"
+            >
+            </ion-img>
+            <div class="menu-info">
+              <div>Loại: Cơm , cơm rang, bún </div>
+              <div>Giá trung bình: 25k-50k</div>
+              <div>Thời gian order: 11h50- 12h30</div>
+              <div>Số lượng giới hạn: 30 suất</div>
+            </div>
+          </ion-card-content>
+          <ion-card-content class="card-title">
+            <ion-button>
+              Đặt
+            </ion-button>
+          </ion-card-content>
+        </ion-card>
+        <ion-card class="item-card">
+          <ion-card-header>
+            <ion-card-title class="card-title">Trưa ngày 22/08</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-img
+                class="img-card"
+                src="https://f18-zpc.zdn.vn/8038687805436259717/f801ad2366f4b4aaede5.jpg"
+                alt="The Wisconsin State Capitol building in Madison, WI at night"
+            >
+            </ion-img>
+            <div class="menu-info">
+              <div>Loại: Cơm , cơm rang, bún </div>
+              <div>Giá trung bình: 25k-50k</div>
+              <div>Thời gian order: 11h50- 12h30</div>
+              <div>Số lượng giới hạn: 30 suất</div>
+            </div>
+          </ion-card-content>
+          <ion-card-content class="card-title">
+            <ion-button>
+              Đặt
+            </ion-button>
+          </ion-card-content>
+        </ion-card>
+      </ion-row>
     </ion-grid>
   </ion-page>
 
@@ -97,5 +171,22 @@ export default defineComponent({
 }
 .card-title {
   text-align: center;
+}
+.menu-info {
+  color: white;
+  font-weight: 400;
+  margin: 10px 0 10px 0;
+}
+.img-card {
+  border-radius: 8px;
+}
+.item-card {
+  border-radius: 20px;
+}
+@media screen and (min-width: 768px) {
+  .item-card {
+    width: 320px;
+    height: 450px;
+  }
 }
 </style>
