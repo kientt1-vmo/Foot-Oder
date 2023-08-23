4454393ec56c20e26c9e7d28eb1128e6baf2971a
trungtn <trungtn@vmodev>
2023-08-23 08:45:15 +0700

    feat: menu order

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,101 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
+  <ion-menu content-id="main-content">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Menu </ion-title>
+      </ion-toolbar>
+      <ion-list class="list">
+          <ion-button class="btn-menu">
+            <ion-label>Trang chủ</ion-label>
+          </ion-button>
+          <ion-button class="btn-menu">
+            <ion-label>Feedback</ion-label>
+          </ion-button>
+          <ion-button class="btn-menu">
+            <ion-label>Khoản nợ</ion-label>
+          </ion-button>
+      </ion-list>
+    </ion-header>
+  </ion-menu>
+  <ion-page id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Menu</ion-title>
       </ion-toolbar>
     </ion-header>
-
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+    <ion-grid>
+      <ion-card class="item-card">
+          <ion-card-header>
+            <ion-card-title class="card-title">Trưa ngày 22/08</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-img
+                class="img-card"
+                src="https://f18-zpc.zdn.vn/8038687805436259717/f801ad2366f4b4aaede5.jpg"
+                alt="The Wisconsin State Capitol building in Madison, WI at night"
+            >
+            </ion-img>
+            <div>
+              <div>Loại : Cơm , cơm rang, bún </div>
+              <div>Giá trung: 25k-50k</div>
+              <div>Thời gian order: 11h50- 12h30</div>
+              <div>Số lượng giới hạn: 30 suất</div>
+            </div>g
+          </ion-card-content>
+          <ion-card-content class="card-title">
+            <ion-button>
+              Đặt
+            </ion-button>
+          </ion-card-content>
+        </ion-card>
+    </ion-grid>
   </ion-page>
+
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonCard, IonGrid, IonCardHeader, IonCardContent,
+          IonPage, IonTitle, IonToolbar, IonItem, IonList , IonButton, IonImg
+        } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonMenu,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonItem,
+    IonList,
+    IonButton,
+    IonCard,
+    IonGrid,
+    IonCardHeader,
+    IonCardContent,
+    IonImg,
+  },
+});
 </script>
-
 <style scoped>
-#container {
+.list {
+  border-top: solid 1px;
+}
+.btn-menu {
+  width: 95%;
+  --background: #7e7e7e;
+  margin: 4px 10px 4px 10px;
+  color: white;
+  font-weight: 700;
+  height: 40px;
+}
+.card-title {
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
 }
 </style>
